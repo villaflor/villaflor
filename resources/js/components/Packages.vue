f34f79d871ecd26dc8a540c97994c1d9ca1f8520
Mark Anthony M. Villaflor <villaflor@live.com>
2021-09-18 22:04:31 -0400

    Add coming soon

--- resources/js/components/Packages.vue
+++ resources/js/components/Packages.vue
@@ -1,24 +1,24 @@
 <template>
     <div>
         <div class="container mx-auto px-8 lg:px-48">
-            <h2 class="text-3xl font-bold">Packages</h2>
-
-            <p class="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit urna ligula, id bibendum tellus
-                efficitur vitae. Fusce felis erat, tristique at felis sed, rutrum luctus ligula. Aenean vel nulla a nunc
-                ullamcorper rhoncus sit amet non lacus. Duis maximus, dui sit amet lobortis pharetra, nisi tellus
-                rhoncus nisi, ac fringilla justo orci ac turpis. Praesent non eleifend mauris, eu viverra leo. Sed
-                consectetur mattis ultrices. Pellentesque sollicitudin sit amet sem non ultrices. Duis tempus vulputate
-                augue, non lacinia leo rutrum at. Suspendisse potenti.
-            </p>
-            <p class="mt-4">
-                In laoreet placerat bibendum. Phasellus at lacinia nisl, in elementum velit. Vivamus a diam vestibulum,
-                elementum elit ac, blandit velit. Phasellus tempus diam a suscipit gravida. Sed ante metus, convallis
-                eget efficitur non, porta ac leo. Fusce tincidunt enim nulla, ut placerat justo mattis et. Sed ornare
-                neque vitae est aliquet imperdiet. Nunc ornare, odio sit amet sollicitudin mollis, odio velit imperdiet
-                nunc, et euismod libero ex quis nulla. Quisque eu quam pretium, egestas nulla ut, consequat erat. Morbi
-                magna elit, lobortis ut tincidunt at, finibus eu arcu.
+            <h2 class="text-3xl font-serif font-bold">Packages</h2>
+            <p class="mt-10 text-justify leading-loose">
+                Coming Soon ...
             </p>
         </div>
     </div>
 </template>
+
+<style scoped>
+.stylistic-quote-mark {
+    font-size: 5rem;
+    right: 100%;
+    @apply mr-2 hidden font-dank-mono text-neutral-500 absolute top-0 leading-none;
+}
+
+@screen sm {
+    .stylistic-quote-mark {
+        @apply block;
+    }
+}
+</style>
