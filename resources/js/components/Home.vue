61ad94d059cf776dc3792f3eaf19834bae9a2918
Mark Anthony M. Villaflor <villaflor@live.com>
2021-09-18 22:03:37 -0400

    Update Home page

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,38 +1,41 @@
 <template>
     <div>
-        <div class="container mx-auto px-8 lg:px-48">
-            <div class="mt-24 mb-48">
-                <h2 class="leading-tight text-7xl font-serif font-bold">Turn my hobby to an Open Source</h2>
-
+        <div class="container mx-auto px-8 lg:px-48 xl:grid xl:grid-cols-2">
+            <div class="mt-24">
+                <h2 class="leading-tight text-7xl font-serif font-bold">Turn my {{ getWord() }} into an Open Source project</h2>
             </div>
 
-            <div>
-                <p class="mt-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit urna ligula, id bibendum tellus
-                    efficitur vitae. Fusce felis erat, tristique at felis sed, rutrum luctus ligula. Aenean vel nulla a nunc
-                    ullamcorper rhoncus sit amet non lacus. Duis maximus, dui sit amet lobortis pharetra, nisi tellus
-                    rhoncus nisi, ac fringilla justo orci ac turpis. Praesent non eleifend mauris, eu viverra leo. Sed
-                    consectetur mattis ultrices. Pellentesque sollicitudin sit amet sem non ultrices. Duis tempus vulputate
-                    augue, non lacinia leo rutrum at. Suspendisse potenti.
-                </p>
-                <p class="mt-4">
-                    In laoreet placerat bibendum. Phasellus at lacinia nisl, in elementum velit. Vivamus a diam vestibulum,
-                    elementum elit ac, blandit velit. Phasellus tempus diam a suscipit gravida. Sed ante metus, convallis
-                    eget efficitur non, porta ac leo. Fusce tincidunt enim nulla, ut placerat justo mattis et. Sed ornare
-                    neque vitae est aliquet imperdiet. Nunc ornare, odio sit amet sollicitudin mollis, odio velit imperdiet
-                    nunc, et euismod libero ex quis nulla. Quisque eu quam pretium, egestas nulla ut, consequat erat. Morbi
-                    magna elit, lobortis ut tincidunt at, finibus eu arcu.
-                </p>
+            <div class="flex flex-col justify-center items-center">
+                <img class="w-64" :src=getHero()>
             </div>
 
         </div>
     </div>
 </template>
 
-<style scoped>
-
-</style>
-
 <script>
-
+export default {
+    data() {
+        return {
+            words: [
+                'passion',
+                'pastime',
+                'hobby'
+            ],
+            hero: [
+                '/assests/package-open.gif',
+                '/assests/php-code.gif',
+                '/assests/code-fork.gif'
+            ],
+        }
+    },
+    methods: {
+        getWord() {
+            return this.words[Math.floor(Math.random() * this.words.length)];
+        },
+        getHero() {
+            return this.hero[Math.floor(Math.random() * this.hero.length)];
+        }
+    }
+}
 </script>
